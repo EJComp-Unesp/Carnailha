cf1479f5546fb26bcf2b545702e6eeea09a0c580
Unknown <luizfilipemonge@outlook.com>
2019-01-06 21:47:13 -0200

    Accomodations feature
    Added carava's testes, routes, models and schema.

---
+++ model/accomodation/index.js
@@ -0,0 +1,24 @@
+let Accomodation = require('./schema').Accomodation;
+
+module.exports.create = async (data) => {
+    let accomodation = new Accomodation(data);
+    return await accomodation.save();
+}   
+
+module.exports.get = async (config = {}) => {
+    return await Accomodation.find(config).exec();
+}
+
+module.exports.getOneById = async (id) => {
+    return await Accomodation.findById(id).exec();
+}
+
+module.exports.update = async (id, data) => {
+    let accomodation = await Accomodation.findById(id).exec();
+    accomodation.set(data);
+    return await accomodation.save();
+}
+
+module.exports.delete = async (id) => {
+    return !! await Accomodation.findByIdAndDelete(id).exec();
+}
