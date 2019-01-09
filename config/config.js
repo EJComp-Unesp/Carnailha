0ae370ebb3b606876883b79d83e33b42edf5c05f
Gilmar Francisco <gilmarfrancisco828@gmail.com>
2019-01-09 19:30:09 -0200

    Add Config file

--- config/config.js
+++ config/config.js
@@ -1,4 +1,17 @@
-const MESSAGE_FILE = 'messages_ptBR.json';
-module.exports = {
-    MESSAGE_FILE
-}
+//Diferenciet test and dev envs
+
+const config = {
+    APP: {
+        port: process.env.PORT || 5000,
+        version: 'v1'
+    },
+    DB: {
+        host: 'carnailha_admin:carna123@ds151943.mlab.com',
+        port: 51943,
+        name: 'carnailha'
+    },
+    MESSAGE_FILE: 'messages_ptBR.json',
+    UPLOAD_FOLDER: './uploads'
+};
+
+module.exports = config;
--- config/server.js
+++ config/server.js
@@ -1,17 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const config = require('./config');
 let app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 //Serves a static directory to get images
-app.use('/images', express.static('./uploads'));
+app.use('/images', express.static(config.UPLOAD_FOLDER));
 
-let port = process.env.PORT || 5000;
-app.listen(port, function(err){
+app.listen(config.APP.port, function(err){
     if(err) console.log(err);
-    console.log(`Listening on ${port}!`);
+    console.log(`Listening on ${config.APP.port}!`);
 });
 
 module.exports = app;
