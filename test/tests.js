f6fb48fa608ba4c050168bc7c133d0f1dc50d8fe
Unknown <luizfilipemonge@outlook.com>
2019-01-10 22:37:20 -0200

    News feature

--- test/tests.js
+++ test/tests.js
@@ -1,10 +1,8 @@
 require('./tests/oauth.test');
 // require('./tests/user.test');
 // require('./tests/faq.test');
-<<<<<<< HEAD
 // require('./tests/caravan.test');
-require('./tests/accommodation.test');
-=======
-require('./tests/caravan.test');
 // require('./tests/image_upload.test');
->>>>>>> master
+// require('./tests/accommodation.test');
+require('./tests/news.test');
+
