0ae3eeea23b69a8a9051f63fc91a647e34209320
Gilmar Francisco <gilmarfrancisco828@gmail.com>
2019-01-19 10:32:34 -0200

    Add init version of static generator
    + Template caravan

---
+++ scripts/build.js
@@ -0,0 +1,50 @@
+var fs = require('fs-extra');
+var path = require('path');
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+let server = require('../../loader.js');
+
+var outputPath = __dirname + '/build_pages';
+let data = {};
+pages = [
+  {
+    name: `caravanas`,
+    getUrl: `caravan`,
+    getArrayName: `caravans`
+  }
+]
+// First delete everything in the build directory.
+console.log('Cleaning previous build...');
+try {
+  for (var file of fs.readdirSync(outputPath)) {
+    fs.removeSync(path.join(outputPath, file));
+  }
+}
+catch (err) {
+  console.log('Error during cleanup: ' + err);
+  process.exit(1);
+}
+
+// Generate each page from the data provided, using the template.
+console.log('Generating pages...');
+pages.forEach(page => {
+  try {
+    chai.request(server)
+      .get(`/api/v1/${page.getUrl}`)
+      .end(function (err, res) {
+        var pageTemplate = require(`./${page.name}`);//Read the template of page
+        data[`${page.getArrayName}`] = res.body[`${page.getArrayName}`];//Save the array of regs
+        fs.writeFileSync(
+          path.join(outputPath, `${page.name}.html`),
+          pageTemplate.generatePage(data));
+      }); //Write the file
+  }
+  catch (err) {
+    console.log('Error during page generation: ' + err);
+    process.exit(1);
+  }
+});
+
+// Process complete.
+console.log('Done!');
